6265b5a431badc7a41522c250128960d8817ed76
cedrvn10 <brochettes40@outlook.fr>
2023-01-22 11:10:49 +0100

    remove folder, new style

--- application/inc_header.php
+++ application/inc_header.php
@@ -1,7 +1,10 @@
-<a href="option.php"></a><img class="image_avatar_petit" src="PHOTO"></a>
-<nav>
-  <ul>
-    <?php /* if (connecte()) { ?>
+<div id="menu">
+  <h1 class="titre">Todolist</h1>
+  <h3>Tout pour se simplifier la vie !</h3>
+  <a href="option.php"></a><img class="image_avatar_petit" src="PHOTO"></a>
+  <nav>
+    <ul>
+      <?php /* if (connecte()) { ?>
         <li><a href="options.php"><span class="texte_lien">Bienvenu <?= $_SESSION['pseudo'] ?></a></span></li>
       <?php } ?>
       <li><a href="index.php"><span class="texte_lien">Index</span></a></li>
@@ -10,11 +13,12 @@
         <li><a href="categories.php"><span class="texte_lien">Catégories des tâches</span></a></li>
         <li><a href="deconnexion.php" onclick="return confirm('Voulez-vous vraiment vous déconnecter ?')"><span class="texte_lien">Deconnexion</span></a></li>
       <?php } else { */ ?>
-    <li><a href="inscription.php"><span class="texte_lien">Inscription</span></a></li>
-    <li><a href="connexion.php"><span class="texte_lien">Connexion</span></a></li>
-    <!-- <?php ?> -->
-  </ul>
-  <?php  /* if (connecte() and !empty($_SESSION['photo'])) { 
+      <li><a href="inscription.php"><span class="texte_lien">Inscription</span></a></li>
+      <li><a href="connexion.php"><span class="texte_lien">Connexion</span></a></li>
+      <!-- <?php ?> -->
+    </ul>
+    <?php  /* if (connecte() and !empty($_SESSION['photo'])) { 
   <a href="option.php"><img id="avatar_image" src="<?= $_SESSION['photo'] ?>"></a>
    } */ ?>
-</nav>
+  </nav>
+</div>
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 How to test the project ?
 ------------------------
-1. Make an alias on the "www" folder
-2. Create a database with an sql.file from "sql_bakcups" folder 
-3. change your connexion settings on "class monSQL" from "includes/php/include_base.php"
+1. Make an alias on the "www" folder with your favorite AMP software
+2. Create a database with a sql.file from "sql_bakcups" folder 
+3. Change your connexion settings on "class monSQL" from "includes/php/include_base.php"
